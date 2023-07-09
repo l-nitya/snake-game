67dbfc500af3329a1a798d4c845334a9162dcb1d
l-nitya <137490100+l-nitya@users.noreply.github.com>
2023-07-10 00:11:16 +0530

    Update index.py

--- snake game/index.py
+++ snake game/index.py
@@ -29,3 +29,27 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
